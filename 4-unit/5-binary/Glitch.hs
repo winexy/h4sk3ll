28a795e45b85b12f786d65e930490571f73e5f02
Olzhas Seitmagambet <sei7magambet@gmail.com>
2020-09-25 23:53:27 +0600

    refactor 4.5 with modules

---
+++ 4-unit/5-binary/Glitch.hs
@@ -0,0 +1,42 @@
+module Glitch (glitchActions) where
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+
+glitchActions :: [BC.ByteString -> IO BC.ByteString]
+glitchActions = [randomReplaceByte
+                ,randomSortSection
+                ,randomReplaceByte
+                ,randomSortSection
+                ,randomReplaceByte]
+
+intToChar :: Int -> Char
+intToChar int = toEnum safeInt
+  where safeInt = int `mod` 255
+
+intToBC :: Int -> BC.ByteString
+intToBC int = BC.pack [intToChar int]
+
+replaceByte :: Int -> Int -> BC.ByteString -> BC.ByteString
+replaceByte loc charVal bytes = mconcat [before,newChar,after]
+  where (before,rest) = BC.splitAt loc bytes
+        after = BC.drop 1 rest
+        newChar = intToBC charVal
+
+randomReplaceByte :: BC.ByteString -> IO BC.ByteString
+randomReplaceByte bytes = do
+  location <- randomRIO (1, BC.length bytes)
+  charVal <- randomRIO (0, 255)
+  return (replaceByte location charVal bytes)
+
+randomSortSection :: BC.ByteString -> IO BC.ByteString
+randomSortSection bytes = do
+  let sectionSize = 25
+  let bytesLength = BC.length bytes
+  start <- randomRIO (0, bytesLength - sectionSize)
+  return (sortSection start sectionSize bytes)
+
+randomCh :: IO Char
+randomCh = do
+  val <- randomRIO (0, 255)
+  return (toEnum val)
